7e07cdb751f8b2860920abb47e03ed47cf660ade
spwJxey <100385865+spwJxey@users.noreply.github.com>
2022-03-14 02:49:58 +0000

    Completed part 5

---
+++ part5.py
@@ -0,0 +1,46 @@
+def m():
+  print("*   *")
+  print("** **")
+  print("* * *")
+  print("*   *")
+  print("*   *")
+  print()
+
+def i():
+  print(" *** ")
+  print("  *  ")
+  print("  *  ")
+  print("  *  ")
+  print(" *** ")
+  print()
+  
+def s():
+  print(" ****")
+  print("*    ")
+  print(" *** ")
+  print("    *")
+  print("**** ")
+  print()
+
+def p():
+  print("**** ")
+  print("*   *")
+  print("*   *")
+  print("**** ")
+  print("*    ")
+  print("*    ")
+  print()
+
+
+
+m()
+i()
+s()
+s()
+i()
+s()
+s()
+i()
+p()
+p()
+i()
